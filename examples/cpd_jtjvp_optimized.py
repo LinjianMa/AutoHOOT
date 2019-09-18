f272715f6bd17d79c0aba28ff242dc868af2de6c
LinjianMa <malinjianev@gmail.com>
2019-09-18 15:43:38 -0500

    add cpd_jtjvp_optimized function

---
+++ examples/cpd_jtjvp_optimized.py
@@ -0,0 +1,52 @@
+import backend as T
+
+
+def compute_coefficient_matrix(n1, n2, G):
+    ret = T.ones(G[0].shape)
+    for i in range(len(G)):
+        if i != n1 and i != n2:
+            ret = T.einsum("ij,ij->ij", ret, G[i])
+    return ret
+
+
+def jtjvp(inputs):
+
+    v_A = inputs[0]
+    B0 = inputs[1]
+    C0 = inputs[2]
+    v_B = inputs[3]
+    A0 = inputs[4]
+    v_C = inputs[5]
+    A = [A0, B0, C0]
+    v = [v_A, v_B, v_C]
+
+    # compute G
+    G = []
+    for i in range(3):
+        G.append(T.einsum("ij,ik->jk", A[i], A[i]))
+
+    # compute gamma
+    gamma = []
+    for i in range(3):
+        gamma.append([])
+        for j in range(3):
+            if j >= i:
+                M = compute_coefficient_matrix(i, j, G)
+                gamma[i].append(M)
+            else:
+                M = gamma[j][i]
+                gamma[i].append(M)
+
+    # fast hessian contract
+    ret = []
+    for n in range(3):
+        ret.append(T.zeros(A[n].shape))
+        for p in range(3):
+            M = gamma[n][p]
+            if n == p:
+                ret[n] += T.einsum("iz,zr->ir", v[p], M)
+            else:
+                B = T.einsum("jr,jz->rz", A[p], v[p])
+                ret[n] += T.einsum("iz,zr,rz->ir", A[n], M, B)
+
+    return [ret[0], ret[1], ret[2]]
