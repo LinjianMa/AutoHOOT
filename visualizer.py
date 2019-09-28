4a1aa3fc9c1357f92aee5326b5440808f328a960
Jiayu Ye <morbyzantine@gmail.com>
2019-09-28 14:27:44 -0700

    Add a graphviz visualizer for the computation graph

---
+++ visualizer.py
@@ -0,0 +1,37 @@
+from graphviz import Digraph
+import autodiff as ad
+import numpy as np
+from utils import einsum_grad_subscripts, find_topo_sort, topo_sort_dfs, sum_node_list, inner_product
+
+a1 = ad.Variable(name="a1", shape=[3, 2])
+a2 = ad.Variable(name="a2", shape=[2, 3])
+
+b1 = ad.Variable(name="b1", shape=[3, 2])
+b2 = ad.Variable(name="b2", shape=[2, 3])
+
+x = ad.einsum('ik,kj->ij', a1, a2)
+y = ad.einsum('jl,ls->js', b1, b2)
+
+z = ad.einsum('ij, js->is', x, y)
+
+executor = ad.Executor([z])
+topo_order = find_topo_sort(executor.eval_node_list)
+
+inputs = filter(lambda x: isinstance(x, ad.VariableNode), topo_order)
+outputs = filter(lambda x: len(x.inputs) == 0, topo_order)
+
+dot = Digraph(comment='Poorman Computation Graph')
+with dot.subgraph() as s:
+    s.attr(rank='same')
+    for n in inputs:
+        s.node(n.name, color='blue')
+with dot.subgraph() as s:
+    s.attr(rank='same')
+    for n in outputs:
+        s.node(n.name, color='red')
+for node in topo_order:
+    dot.node(node.name, node.name)
+    for node_i in node.inputs:
+        dot.edge(node_i.name, node.name)
+
+print(dot.source)
