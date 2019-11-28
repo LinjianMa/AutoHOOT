5ed40263da19d80cd039509c93a071de96ca85d3
Jiayu Ye <morbyzantine@gmail.com>
2019-11-27 23:36:09 -0800

    Cleanup benchmark test

--- tests/cpd_benchmark_test.py
+++ tests/cpd_benchmark_test.py
@@ -69,29 +69,7 @@ def test_cpd_jtjvp(benchmark):
     for datatype in BACKEND_TYPES:
         T.set_backend(datatype)
 
-        A, B, C, input_tensor, loss, residual = cpd_graph(size, rank)
-        v_A = ad.Variable(name="v_A", shape=[size, rank])
-        v_B = ad.Variable(name="v_B", shape=[size, rank])
-        v_C = ad.Variable(name="v_C", shape=[size, rank])
-
         A_val, B_val, C_val, input_tensor_val = init_rand_3d(size, rank)
         v_A_val, v_B_val, v_C_val, _ = init_rand_3d(size, rank)
-
-        JtJvps = ad.jtjvps(output_node=residual,
-                           node_list=[A, B, C],
-                           vector_list=[v_A, v_B, v_C])
-        executor_JtJvps = ad.Executor(JtJvps)
-
-        jtjvp_val = executor_JtJvps.run(
-            feed_dict={
-                A: A_val,
-                B: B_val,
-                C: C_val,
-                input_tensor: input_tensor_val,
-                v_A: v_A_val,
-                v_B: v_B_val,
-                v_C: v_C_val
-            })
-
         expected_hvp_val = benchmark(expect_jtjvp_val, A_val, B_val, C_val,
                                      v_A_val, v_B_val, v_C_val)
