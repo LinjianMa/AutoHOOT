18221bb79d693b4a2f915688a7e931e2ee7b71a4
Jiayu Ye <morbyzantine@gmail.com>
2019-12-30 23:13:19 -0800

    Let rewrite einsum expr handle the case where input node order are different

--- tests/graph_transforms_test.py
+++ tests/graph_transforms_test.py
@@ -337,3 +337,18 @@ def test_rewrite_expr():
     rewrite_einsum_expr(x)
     rewrite_einsum_expr(y)
     assert x.einsum_subscripts == y.einsum_subscripts
+
+
+def test_einsum_equal():
+
+    a1 = ad.Variable(name="a1", shape=[3, 2])
+    a2 = ad.Variable(name="a2", shape=[2, 3])
+
+    x = ad.einsum('ik,kj->ij', a1, a2)
+    y = ad.einsum('ml,sm->sl', a2, a1)
+
+    rewrite_einsum_expr(x)
+    rewrite_einsum_expr(y)
+
+    assert x.einsum_subscripts == y.einsum_subscripts
+    assert x.inputs == y.inputs
