6f0e4c8293f23ea44e1ec9027b32f4f5beadb3f6
LinjianMa <malinjianev@gmail.com>
2020-02-08 21:01:12 -0600

    tucker als fuction, open for discussion

--- examples/tucker.py
+++ examples/tucker.py
@@ -1,6 +1,7 @@
 import autodiff as ad
 import backend as T
 from utils import CharacterGetter
+from tensors.synthetic_tensors import init_rand_tucker
 
 BACKEND_TYPES = ['numpy']
 
@@ -49,6 +50,43 @@ def tucker_graph(dim, size, rank):
     residual = output - X
     loss = ad.einsum(f"{X_subscripts},{X_subscripts}->", residual, residual)
     return A_list, core, X, loss, residual
+
+
+def tucker_als(dim, size, rank, num_iter, input_val=[]):
+    A_list, core, X, loss, residual = tucker_graph(dim, size, rank)
+
+    executors = []
+
+    for i in range(dim):
+
+        core_A = ad.intermediate(loss, {core, A_list[i]})
+        hes = ad.hessian(loss, core_A)
+        grad = ad.gradients(loss, core_A)
+
+        new_core_A = core_A - ad.tensordot(
+            ad.tensorinv(hes), grad,
+            [[i + dim for i in range(dim)], [i for i in range(dim)]])
+
+        executor = ad.Executor([loss, new_core_A])
+        executors.append(executor)
+
+    if input_val == []:
+        A_list_val, core_val, X_val = init_rand_tucker(dim, size, rank)
+    else:
+        A_list_val, core_val, X_val = input_val
+
+    for iter in range(num_iter):
+        # als iterations
+        for i in range(dim):
+
+            loss_val, new_core_A_val = executors[i].run(feed_dict={  #TODO
+            })
+
+            # update core and A_i
+
+        print(f'At iteration {iter} the loss is: {loss_val}')
+
+    return A_val, B_val, C_val
 
 
 if __name__ == "__main__":
