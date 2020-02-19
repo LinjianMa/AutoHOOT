dbcb57e3c39af23daa33d08b37dd8fbdbe8f2f7a
LinjianMa <malinjianev@gmail.com>
2020-02-19 11:15:25 -0600

    remove the inv optimization w.r.t. unequality cuz it involves invertability of a matrix

--- graph_ops/graph_inv_optimizer.py
+++ graph_ops/graph_inv_optimizer.py
@@ -3,6 +3,7 @@
     tensor inverse.
 """
 import logging
+import numpy as np
 import autodiff as ad
 from utils import PseudoNode
 
@@ -17,37 +18,7 @@ logging.basicConfig(format=FORMAT)
 logger.setLevel(logging.DEBUG)
 
 
-def find_one_subset_sum(arr, n, sum, indices=[]):
-    """
-    Find one subset of the input array with given sum.
-
-    Parameters
-    ----------
-    arr: The input array.
-    n: The cursor position. n should be int within [0, len(arr)].
-    sum: int, the given sum of the sebset.
-    indices: the input indices.
-
-    Returns
-    -------
-    A list representing the indices of the subset.
-    """
-
-    if sum == 0 and indices != []:
-        return indices
-    if n == 0:
-        return []
-
-    new_indices = find_one_subset_sum(arr, n - 1, sum, indices)
-    if new_indices != []:
-        return new_indices
-
-    new_indices = indices.copy()
-    new_indices.append(n - 1)
-    return find_one_subset_sum(arr, n - 1, sum - arr[n - 1], new_indices)
-
-
-def inv_disjoint_sets(p_einsum_node, p_in_nodes, uf):
+def inv_disjoint_sets(p_einsum_node, p_in_nodes):
     """
     Get the disjoint sets for inverse optimization.
 
@@ -55,7 +26,6 @@ def inv_disjoint_sets(p_einsum_node, p_in_nodes, uf):
     ----------
     p_einsum_node: The PseudoNode for the einsum node (inv node input).
     p_in_nodes: The PseudoNode of inputs for the einsum node.
-    uf: The union find class for subscripts of the einsum node.
 
     Returns
     -------
@@ -63,46 +33,21 @@ def inv_disjoint_sets(p_einsum_node, p_in_nodes, uf):
     that can be decomposable from other parts for the inverse.
 
     """
+    whole_str = p_einsum_node.subscript + "".join(
+        [pnode.subscript for pnode in p_in_nodes])
+    uf = UF(list(whole_str))
 
+    # Each input node must be in its own disjoint set
     for node in p_in_nodes:
         for char in node.subscript:
             uf.connect(node.subscript[0], char)
 
     matrix_dim = int(len(p_einsum_node.node.shape) / 2)
 
-    # For each dim in the matrix col, connect it to the corresponding dim
-    # in the matrix row.
+    # For each dim in the matrix col, connect it to the corresponding dim in the matrix row.
     for i in range(matrix_dim):
         uf.connect(p_einsum_node.subscript[i],
                    p_einsum_node.subscript[i + matrix_dim])
-
-    # For the case when the size of one dim of the matrix col is different with the
-    # correspoding dim size of the matrix row, we connect it with some other dims, such
-    # that for each decomposable set, the corresponding matrix is squared.
-    shape_diff = [
-        p_einsum_node.node.shape[i] - p_einsum_node.node.shape[i + matrix_dim]
-        for i in range(matrix_dim)
-    ]
-    shape_unequal_tuple = [(i, v) for (i, v) in enumerate(shape_diff)
-                           if v != 0]
-
-    while shape_unequal_tuple != []:
-
-        _, shape_diff = list(zip(*shape_unequal_tuple))
-        indices = find_one_subset_sum(shape_diff, len(shape_diff), 0)
-
-        subset = [
-            v for (i, v) in enumerate(shape_unequal_tuple) if i in indices
-        ]
-        shape_unequal_tuple = [
-            v for (i, v) in enumerate(shape_unequal_tuple) if i not in indices
-        ]
-
-        # connect subscripts in the subset
-        indices, _ = list(zip(*subset))
-        for i in indices:
-            uf.connect(p_einsum_node.subscript[i],
-                       p_einsum_node.subscript[indices[0]])
 
     return uf.disjoint_set()
 
@@ -130,9 +75,28 @@ def optimize_inverse(inv_node):
         new_einsum = ad.einsum(new_subscripts, *inputs)
         return new_einsum
 
-    # Note: currently, the optimization algorithm only works for the
-    # case when the matrix row and column has same number of dimension.
+    # Note: currently, the optimization algorithm only works when
+    # 1. the matrix row and column has same number of dimension,
+    # 2. the matrix is square,
+    # 3. each corresponding dimension in row and column has the same size.
     if inv_node.input_indices_length * 2 != len(inv_node.shape):
+        logger.info(f"Dimension length doesn't agree, can't optimize inverse")
+        return inv_node
+    matrix_dim = int(len(inv_node.shape) / 2)
+
+    if np.prod(inv_node.shape[:matrix_dim]) != np.prod(
+            inv_node.shape[matrix_dim:]):
+        logger.info(f"The matrix is not square, can't optimize inverse")
+        return inv_node
+
+    shape_diff_list = [
+        inv_node.shape[i] - inv_node.shape[i + matrix_dim]
+        for i in range(matrix_dim)
+    ]
+    if any(shape_diff != 0 for shape_diff in shape_diff_list):
+        logger.info(
+            f"Each corresponding dimension in row and column doesn't have the same size, can't optimize inverse"
+        )
         return inv_node
 
     assert isinstance(inv_node, ad.TensorInverseNode)
@@ -145,28 +109,23 @@ def optimize_inverse(inv_node):
     in_subs, out_subs, _ = _parse_einsum_input(
         (einsum_node.einsum_subscripts, *einsum_node.inputs))
     in_subs_list = in_subs.split(',')
-    whole_str = out_subs + "".join(in_subs_list)
-
-    uf = UF(list(whole_str))
 
     p_einsum_node = PseudoNode(node=einsum_node, subscript=out_subs)
     p_in_nodes = []
     for i, node in enumerate(einsum_node.inputs):
         p_in_nodes.append(PseudoNode(node=node, subscript=in_subs_list[i]))
 
-    dsets = inv_disjoint_sets(p_einsum_node, p_in_nodes, uf)
+    dsets = inv_disjoint_sets(p_einsum_node, p_in_nodes)
 
-    # if the node cannot be decomposed, just return
-    # the input node
+    # If the node cannot be decomposed, just return the input node
     if len(dsets) == 1:
         return inv_node
 
     new_inputs = []
     for dset in dsets:
         input_decomp_einsum = list(
-            filter(
-                lambda node: not all(char not in dset
-                                     for char in node.subscript), p_in_nodes))
+            filter(lambda node: any(char in dset for char in node.subscript),
+                   p_in_nodes))
         out_subs = "".join(
             [char for char in p_einsum_node.subscript if char in dset])
 
