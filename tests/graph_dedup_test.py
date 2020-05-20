a44054a98c7f76a2546f82fe3f292e1de6093d07
Linjian Ma <malinjianev@gmail.com>
2020-05-19 22:51:35 -0500

    replace literals with structured dim information in optimization module (#209)

--- tests/graph_dedup_test.py
+++ tests/graph_dedup_test.py
@@ -97,7 +97,9 @@ def test_get_transpose_indices():
     assert get_transpose_indices(ad.einsum('acb,bd->adc', a, b),
                                  ad.einsum('dab,bc->dac', a, b)) == [0, 2, 1]
     assert get_transpose_indices(ad.einsum('acje,ie->iacj', c, b),
-                                 ad.einsum('jace,ie->iacj', c, b)) == [0, 2, 3, 1]
+                                 ad.einsum('jace,ie->iacj', c,
+                                           b)) == [0, 2, 3, 1]
+
 
 def test_remove_transposes():
     a = ad.Variable(name="a", shape=[2, 2, 2, 2])
